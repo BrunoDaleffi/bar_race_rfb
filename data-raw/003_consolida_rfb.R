8642ff323fd474719240547b63758a85387edf12
Bruno Daldeffi <brunodaleffi@gmail.com>
2022-10-19 12:28:18 -0300

    nome do cnae_id

--- data-raw/003_consolida_rfb.R
+++ data-raw/003_consolida_rfb.R
@@ -2,7 +2,7 @@ library(tidyverse)
 
 rfb_consolidada <- readr::read_rds('data-raw/rfb/rds/rfb.rds') %>%
   dplyr::mutate(ano_mes = lubridate::floor_date(dt_inicio,'month')) %>%
-  dplyr::group_by(ano_mes,cnae_id,cnae_nm) %>%
+  dplyr::group_by(ano_mes,cnae_id = cnae_principal,cnae_nm) %>%
   dplyr::summarise(n = dplyr::n())
 
 readr::write_rds(rfb_consolidada,'data/rfb_consolidada.rds',compress = 'xz')
